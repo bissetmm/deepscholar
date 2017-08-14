54b945d31eea085686b178f04696a957f83242cd
Taichiro Yoshida <taichiro.yoshida@gmail.com>
2017-08-14 23:08:27 +0900

    Enable search feature on detail page

--- client/src/components/Root/index.js
+++ client/src/components/Root/index.js
@@ -7,28 +7,74 @@ import Detail from '../Detail';
 import './style.css';
 import queryString from 'query-string';
 
-class Root extends Component {
+class NavBar extends Component {
   constructor(props) {
     super(props);
     this.searchTimer = null;
-    const parsed = queryString.parse(window.location.search);
-    this.state = {query: parsed.q};
+    this.query = null;
   }
 
-  handleTextChange(e) {
+  handleSubmit(e) {
+    e.preventDefault();
+
     if (this.searchTimer !== null) {
       clearTimeout(this.searchTimer);
       this.searchTimer = null;
     }
-    const query = e.target.value;
+    const query = this.query;
 
     if (!query) {
       return;
     }
 
     this.searchTimer = setTimeout(() => {
-      this.setState({query: query});
-    }, 1000);
+      this.props.history.push(`/?q=${query}`);
+      this.props.performSearch(query);
+    }, 0);
+  }
+
+  handleChange(e) {
+    this.query = e.target.value;
+  }
+
+  render() {
+    return (
+      <div className="nav-wrapper">
+        <div className="row">
+          <div className="col s4 l3 hide-on-med-and-down">
+            <Link to="/" className="brand-logo"><i className="material-icons">cloud</i>DeepScholar</Link>
+          </div>
+          <div className="col s9 l7">
+            <div className="input-field">
+              <form onSubmit={this.handleSubmit.bind(this)}>
+                <input type="search" placeholder="Word Representations" onChange={this.handleChange.bind(this)} defaultValue={this.props.query}/>
+              </form>
+              <label className="label-icon" htmlFor="search"><i className="material-icons">search</i>
+              </label>
+              <i className="material-icons closed">close</i>
+            </div>
+          </div>
+          <div className="col s3 l2 right">
+            <ul className="right">
+              <li><a href="#"><i className="material-icons">view_module</i></a></li>
+              <li><a href="#"><i className="material-icons">refresh</i></a></li>
+              <li><a href="#"><i className="material-icons">more_vert</i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+class Root extends Component {
+  constructor(props) {
+    super(props);
+    const parsed = queryString.parse(window.location.search);
+    this.state = {query: parsed.q};
+  }
+
+  performSearch(query) {
+    this.setState({query: query});
   }
 
   render() {
@@ -37,29 +83,9 @@ class Root extends Component {
         <div>
           <div className="navbar-fixed">
             <nav className="navy">
-              <div className="nav-wrapper">
-                <div className="row">
-                  <div className="col s4 l3 hide-on-med-and-down">
-                    <Link to="/" className="brand-logo"><i className="material-icons">cloud</i>DeepScholar</Link>
-                  </div>
-                  <div className="col s9 l7">
-                    <div className="input-field">
-                      <input type="search" onChange={this.handleTextChange.bind(this)}
-                             placeholder="Word Representations"/>
-                      <label className="label-icon" htmlFor="search"><i className="material-icons">search</i>
-                      </label>
-                      <i className="material-icons closed">close</i>
-                    </div>
-                  </div>
-                  <div className="col s3 l2 right">
-                    <ul className="right">
-                      <li><a href="#"><i className="material-icons">view_module</i></a></li>
-                      <li><a href="#"><i className="material-icons">refresh</i></a></li>
-                      <li><a href="#"><i className="material-icons">more_vert</i></a></li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
+              <Route component={(props) => (
+                <NavBar {...props} performSearch={this.performSearch.bind(this)} query={this.state.query}/>
+              )}/>
             </nav>
           </div>
           <div className="container">
