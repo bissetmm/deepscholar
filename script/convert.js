31130a2622ae148b618752359fdc0a7b2aa355b0
Taichiro Yoshida <taichiro.yoshida@gmail.com>
2017-11-14 23:27:51 +0900

    Change convert script to match latest sample xml

--- script/convert.js
+++ script/convert.js
@@ -1,8 +1,9 @@
 const {parseString} = require('fast-xml2js');
-const camelcaseKeys = require('camelcase-keys');
+const camelcase = require('camelcase');
 const fs = require('fs');
 const path = require('path');
 const glob = require("glob");
+const util = require("util");
 
 const dirPath = process.argv[2];
 
@@ -10,13 +11,22 @@ const indexMetaData = {
   _index: "documents",
   _type: "lang"
 };
+const shouldBeArray = [
+  "author",
+  "p",
+  "fig"
+];
 
 glob(`${dirPath}/*`, (error, files) => {
   if (error) {
     console.log(error);
     return;
   }
-  files.forEach((filePath) => {
+  files.forEach((dirPath) => {
+    const id = path.basename(dirPath);
+    const fileName = `${id}.xml`;
+    const filePath = path.join(dirPath, fileName);
+
     fs.readFile(filePath, 'utf8', (error, xml) => {
       if (error) {
         console.log(error);
@@ -29,33 +39,74 @@ glob(`${dirPath}/*`, (error, files) => {
           throw error;
         }
 
-        const data = camelcaseKeys(result.article, {deep: true});
+        const data = result.article;
 
-        const id = path.basename(filePath, '.xml');
         const actionAndMetaData = {index: Object.assign(indexMetaData, {_id: id})};
-        data.id = id;
 
-        //Array to single string
-        data["articleTitle"] = data["articleTitle"][0];
-        data["abstract"] = data["abstract"][0];
-        if (data.author) {
-          data.author = data.author.map(value => {
-            const surname = value.surname[0];
-            const givenNames = value.givenNames[0];
-            return {surname, givenNames};
-          });
-        }
+        data.front.id = id.replace("journal.pone.", "");
+        convertObject(data);
 
         console.log(JSON.stringify(actionAndMetaData));
-        console.log(JSON.stringify(data, (key, value) => {
-          // Remove line breaks on head and tail that are generates by fast-xml2js.
-          if (typeof value === 'string') {
-            return value.trim();
-          }
-
-          return value;
-        }));
+        console.log(JSON.stringify(data));
       });
     });
   });
 });
+
+function convertObject(d, oldKey) {
+  const newKey = oldKey ? camelcase(oldKey) : null;
+
+  // convert key to camel case
+  // "journal-title": [
+  //   "PLoS ONE"
+  // ]
+  // to
+  // "journalTitle": [
+  //   "PLoS ONE"
+  // ]
+  if (newKey && newKey !== oldKey) {
+    d[newKey] = d[oldKey];
+    delete d[oldKey];
+  }
+
+  let value = newKey ? d[newKey] : d;
+
+  // convert array to single value
+  // "journalTitle": [
+  //   "PLoS ONE"
+  // ]
+  // to
+  // "journalTitle": "PLoS ONE"
+  if (util.isArray(value) && shouldBeArray.indexOf(newKey) === -1) {
+    d[newKey] = value[0];
+    value = d[newKey];
+  }
+
+  // Remove line breaks on head and tail that are generates by fast-xml2js.
+  if (typeof value === 'string') {
+    d[newKey] = value.trim();
+  }
+
+  // move keys inside attributes($)
+  // {
+  //   "$": {
+  //     "img": "journal.pone.0070001_1.png"
+  //   }
+  // }
+  // to
+  // {
+  //   "img": "journal.pone.0070001_1.png"
+  // }
+  if (newKey === "$") {
+    Object.keys(value).forEach(key => {
+      d[key] = value[key];
+    });
+    delete d["$"];
+  }
+
+  if (util.isObject(value)) {
+    Object.keys(value).forEach(key => {
+      convertObject(value, key);
+    });
+  }
+}
